02366aa78ba6d960e2f5bedd46cc9b65249a8819
bookinsman <vladas272@gmail.com>
2025-11-28 20:10:06 +0200

    aa

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@texuddy/ui', '@texuddy/utils', '@texuddy/types'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 }
 
 module.exports = nextConfig
